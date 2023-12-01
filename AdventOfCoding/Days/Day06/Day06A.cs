de682a1cf9bff52e5f3e47a2390da6a97ead27b2
KaNaDaAT <kalian.danzer@gmail.com>
2023-12-01 14:54:24 +0100

    AOC 2023 Initial commit

---
+++ AdventOfCoding/Days/Day06/Day06A.cs
@@ -0,0 +1,25 @@
+﻿using Lib;
+using System;
+using System.Diagnostics;
+
+namespace AdventOfCoding.Days {
+	public class Day06A : DayAbstract {
+
+		protected override void Runner(Reader reader) {
+			string[] lines = reader.ReadAndGetLines();
+
+
+			for(int i = 0; i < lines.Length; i++) {
+				
+			}
+
+			this.Result = null;
+
+			/* Output Region */
+			if (!IsDebugMode)
+				return;
+			this.ToPrint.AppendLine("");
+		}
+
+	}
+}
