d51cc52af39ea288e74cc07a141ecaaeea48259a
KaNaDaAT <kalian.danzer@gmail.com>
2023-12-02 19:56:29 +0100

    aoc: Day02B

--- AdventOfCoding/Days/Day02/Day02B.cs
+++ AdventOfCoding/Days/Day02/Day02B.cs
@@ -1,24 +1,25 @@
 ﻿using Lib;
-using System;
-using System.Diagnostics;
+using System.Linq;
 
 namespace AdventOfCoding.Days {
 	public class Day02B : DayAbstract {
 
-		protected override void Runner(Reader reader) {
-			string[] lines = reader.ReadAndGetLines();
-
-
-			for(int i = 0; i < lines.Length; i++) {
-				
-			}
-
-			this.Result = null;
-
-			/* Output Region */
-			if (!IsDebugMode)
-				return;
-			this.ToPrint.AppendLine("");
+		protected override void Runner(Reader reader)
+		{
+			this.Result = reader.ReadAndGetLines()
+				.Select(_ => _.Split(':'))
+				.Select(_ => _[1].Split(';')
+					.Select(_ => _.Split(',')
+						.Select(_ => _.Trim().Split(' '))
+						.ToNullableDictionary(_ => _[1], _ => (int?)int.Parse(_[0]))
+					)
+				)
+				.Select(_ =>
+						_.Max(_ => _["red"] ?? 0) *
+						_.Max(_ => _["green"] ?? 0) *
+						_.Max(_ => _["blue"] ?? 0)
+				)
+				.Sum();
 		}
 
 	}
