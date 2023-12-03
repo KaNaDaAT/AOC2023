2ba15767109725620ac32aa46dcd6d8022e7a3d9
KaNaDaAT <kalian.danzer@gmail.com>
2023-12-03 07:33:16 +0100

    aoc: Day03B

--- AdventOfCoding/Days/Day03/Day03B.cs
+++ AdventOfCoding/Days/Day03/Day03B.cs
@@ -1,24 +1,33 @@
 ﻿using Lib;
-using System;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCoding.Days {
 	public class Day03B : DayAbstract {
 
-		protected override void Runner(Reader reader) {
-			string[] lines = reader.ReadAndGetLines();
+		protected override void Runner(Reader reader)
+		{
+			var grid = new Grid<char>(reader.ReadAndGetLines().Select(_ => _.ToCharArray()).ToArray());
 
-
-			for(int i = 0; i < lines.Length; i++) {
-				
-			}
-
-			this.Result = null;
-
-			/* Output Region */
-			if (!IsDebugMode)
-				return;
-			this.ToPrint.AppendLine("");
+			var gears = new NullableDictionary<(int X, int Y), List<int>>();
+			grid.ForEach(_ => {
+				if (!char.IsDigit(grid.Get(_.x, _.y)) || char.IsDigit(grid.Get(_.x - 1, _.y, '1')))
+					return;
+				List<(int x, int y, int value)> numbers = new() { (_.x, _.y, grid.Get(_.x, _.y) - '0') };
+				for (int i = 1; i < grid.ColumnSize - _.x; i++)
+				{
+					var right = grid.Get(_.x + i, _.y, ' ');
+					if (!char.IsDigit(right)) break;
+					numbers.Add((_.x + i, _.y, right - '0'));
+				}
+				var number = numbers.Select(_ => _.value).Aggregate((acc, num) => acc * 10 + num);
+				numbers.SelectMany(_ => grid.GetAdjcentWithIndex(_.x, _.y, false))
+					.Distinct()
+					.Where(_ => _.Value == '*')
+					.ToList()
+					.ForEach(_ => (gears[(_.X, _.Y)] ??= new List<int>()).Add(number));
+			});
+			this.Result = gears.Where(_ => _.Value.Count == 2).Select(_ => _.Value[0] * _.Value[1]).Sum();
 		}
 
 	}
