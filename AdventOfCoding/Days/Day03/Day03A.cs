892b9a27e1518c14adb310fc9c2a048cc8da192f
KaNaDaAT <kalian.danzer@gmail.com>
2023-12-03 07:33:08 +0100

    aoc: Day03A

--- AdventOfCoding/Days/Day03/Day03A.cs
+++ AdventOfCoding/Days/Day03/Day03A.cs
@@ -1,24 +1,30 @@
 ﻿using Lib;
-using System;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCoding.Days {
 	public class Day03A : DayAbstract {
 
-		protected override void Runner(Reader reader) {
-			string[] lines = reader.ReadAndGetLines();
+		protected override void Runner(Reader reader)
+		{
+			var grid = new Grid<char>(reader.ReadAndGetLines().Select(_ => _.ToCharArray()).ToArray());
 
-
-			for(int i = 0; i < lines.Length; i++) {
-				
-			}
-
-			this.Result = null;
-
-			/* Output Region */
-			if (!IsDebugMode)
-				return;
-			this.ToPrint.AppendLine("");
+			this.Result = grid.Select(_ => {
+				if (!char.IsDigit(grid.Get(_.x, _.y)) || char.IsDigit(grid.Get(_.x - 1, _.y, '1')))
+					return 0;
+				List<(int x, int y, int value)> numbers = new() { (_.x, _.y, grid.Get(_.x, _.y) - '0') };
+				for (int i = 1; i < grid.ColumnSize - _.x; i++)
+				{
+					var right = grid.Get(_.x + i, _.y, ' ');
+					if (!char.IsDigit(right)) break;
+					numbers.Add((_.x + i, _.y, right - '0'));
+				}
+				var count = numbers.SelectMany(_ => grid.GetAdjcentWithIndex(_.x, _.y, false))
+					.Distinct()
+					.Count(_ => _.Value != '.' && !char.IsDigit(_.Value));
+				this.ToPrint.AppendLine(count + ": " + numbers.Select(_ => _.value).Aggregate((acc, num) => acc * 10 + num));
+				return numbers.Select(_ => _.value).Aggregate((acc, num) => acc * 10 + num) * count;
+			}).Sum();
 		}
 
 	}
