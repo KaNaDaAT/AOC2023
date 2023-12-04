b3a5560d01d1c0c82629e6ce7fa1cba5b78fc617
KaNaDaAT <kalian.danzer@gmail.com>
2023-12-04 23:18:21 +0100

    aoc: Day04B

--- AdventOfCoding/Days/Day04/Day04B.cs
+++ AdventOfCoding/Days/Day04/Day04B.cs
@@ -1,24 +1,44 @@
 ﻿using Lib;
 using System;
-using System.Diagnostics;
+using System.Linq;
 
 namespace AdventOfCoding.Days {
 	public class Day04B : DayAbstract {
 
-		protected override void Runner(Reader reader) {
-			string[] lines = reader.ReadAndGetLines();
+		class Card {
+			public long Count;
+			public int Worth;
+			public Card(int worth) { Count = 1; Worth = worth; }
+		}
 
+		protected override void Runner(Reader reader)
+		{
+			this.Result = reader.ReadAndGetLines()
+				.Select(line => line.Split(':')[1].Trim().Split(" | "))
+				.Select(pair => new Card(pair[0].Split(" ", (StringSplitOptions)1).Intersect(pair[1].Split(" ", (StringSplitOptions)1)).Count()))
+				.ToList()
+				.SelectSubset(
+					(card, index, list) => list.Skip(index + 1).Take(card.Worth).ToList(),
+					(card, index, next) => {
+						next.Select(_ => _.Count += card.Count).ToList();
+						return card.Count;
+					}
+				)
+				.Sum();
+		}
 
-			for(int i = 0; i < lines.Length; i++) {
-				
+		public void MoreReadableVersion(Reader reader)
+		{
+
+			var list = reader.ReadAndGetLines()
+				.Select(line => line.Split(':')[1].Trim().Split(" | "))
+				.Select(pair => new Card(pair[0].Split(" ", (StringSplitOptions)1).Intersect(pair[1].Split(" ", (StringSplitOptions)1)).Count()))
+				.ToList();
+			for (int i = 0; i < list.Count(); i++)
+			{
+				list.Skip(i + 1).Take(list[i].Worth).Select((card, index) => card.Count += list[i].Count).ToList();
 			}
-
-			this.Result = null;
-
-			/* Output Region */
-			if (!IsDebugMode)
-				return;
-			this.ToPrint.AppendLine("");
+			this.Result = list.Sum(_ => _.Count);
 		}
 
 	}
---
+++ Lib/ListExtensions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lib {
+	public static class ListExtensions {
+
+		public static IEnumerable<TResult> SelectSubset<TSource, TResult>(
+			this IEnumerable<TSource> source,
+			Func<TSource, bool> subsetSelector,
+			Func<TSource, List<TSource>, TResult> selector
+		)
+		{
+			ArgumentNullException.ThrowIfNull(source);
+			ArgumentNullException.ThrowIfNull(subsetSelector);
+			ArgumentNullException.ThrowIfNull(selector);
+
+			var list = source.Where(_ => subsetSelector.Invoke(_)).ToList();
+			return source.Select(_ => selector.Invoke(_, list));
+		}
+
+		public static IEnumerable<TResult> SelectSubset<TSource, TResult>(
+			this IEnumerable<TSource> source,
+			Func<TSource, int, bool> subsetSelector,
+			Func<TSource, List<TSource>, TResult> selector
+		)
+		{
+			ArgumentNullException.ThrowIfNull(source);
+			ArgumentNullException.ThrowIfNull(subsetSelector);
+			ArgumentNullException.ThrowIfNull(selector);
+
+			var list = source.Where(subsetSelector.Invoke).ToList();
+			return source.Select(_ => selector.Invoke(_, list));
+		}
+
+		public static IEnumerable<TResult> SelectSubset<TSource, TResult>(
+			this IEnumerable<TSource> source,
+			Func<IEnumerable<TSource>, IEnumerable<TSource>> subsetSelector,
+			Func<TSource, List<TSource>, TResult> selector
+		)
+		{
+			ArgumentNullException.ThrowIfNull(source);
+			ArgumentNullException.ThrowIfNull(subsetSelector);
+			ArgumentNullException.ThrowIfNull(selector);
+
+			var list = subsetSelector(source).ToList();
+			return source.Select(_ => selector.Invoke(_, list));
+		}
+
+		public static IEnumerable<TResult> SelectSubset<TSource, TResult>(
+			this IEnumerable<TSource> source,
+			Func<TSource, int, IEnumerable<TSource>, IEnumerable<TSource>> subsetSelector,
+			Func<TSource, int, IEnumerable<TSource>, TResult> selector
+		)
+		{
+			ArgumentNullException.ThrowIfNull(source);
+			ArgumentNullException.ThrowIfNull(subsetSelector);
+			ArgumentNullException.ThrowIfNull(selector);
+
+			return source.Select((el, index) => selector(el, index, subsetSelector(el, index, source)));
+		}
+
+		public static IEnumerable<TResult> SelectSubset<TSource, TResult>(
+			this IEnumerable<TSource> source,
+			int from,
+			int to,
+			Func<TSource, List<TSource>, TResult> selector
+		)
+		{
+			ArgumentNullException.ThrowIfNull(source);
+			ArgumentNullException.ThrowIfNull(selector);
+
+			var list = source.Skip(from).Take(to - from).ToList();
+			return source.Select(_ => selector.Invoke(_, list));
+		}
+	}
+}
